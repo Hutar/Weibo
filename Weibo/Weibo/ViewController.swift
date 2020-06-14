9010c450d7f4d15a438df37cd7c3f1326113a989
Hutar <tlrr@outlook.com>
2020-06-14 21:11:17 +0800

    "edit pods"

---
+++ Weibo/Weibo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Weibo
+//
+//  Created by Criston Oring on 2020/6/14.
+//  Copyright © 2020 Santious. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
